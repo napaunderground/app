6d7e4478d391d7c55dc119dca490abd10d32044f
nathan <napaunderground@gmail.com>
2015-04-14 17:47:59 -0700

    example

---
+++ src/main/java/nss/learndatabase/CommentModel.java
@@ -0,0 +1,30 @@
+package nss.learndatabase;
+
+/**
+ * Created by nathan on 4/11/15.
+ */
+public class CommentModel {
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
---
+++ src/main/java/nss/learndatabase/TestDatabaseActivity.java
@@ -0,0 +1,73 @@
+package nss.learndatabase;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+import java.util.Random;
+
+public class TestDatabaseActivity extends ListActivity {
+    private CommentsDataSource datasource;
+
+
+
+    // this runs hello world..... ?P
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_);
+
+        datasource = new CommentsDataSource(this);
+        datasource.open();
+
+        List<CommentModel> values = datasource.getAllComments();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<CommentModel> adapter = new ArrayAdapter<CommentModel>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<CommentModel> adapter = (ArrayAdapter<CommentModel>) getListAdapter();
+        CommentModel comment = null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
+                int nextInt = new Random().nextInt(3);
+                // save the new comment to the database
+                comment = datasource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    comment = (CommentModel) getListAdapter().getItem(0);
+                    datasource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+}
